e14160d4fd57cd896b52e7850617421dd3c81a4a
transsuperfreak <168235283+transsuperfreak@users.noreply.github.com>
2024-05-05 13:34:50 +0200

    Update init.js

--- init.js
+++ init.js
@@ -7,14 +7,17 @@
   el.onload = function() {
     var socket = io(url);
     var spymode;
-spymode = "t"
+spymode = "f"
+var setname = "Testbot :3 [!help!]"
+                         
+    var room = "atrium";
 
 
     socket.on('connect', function() {
       
       socket.emit('user joined', 'Testbot :3 [!help!]', '', '', '', '');
       socket.on('user joined', function(data) {
-		
+		$log(data)
       $log(data.nick + " joined")
 
 			
@@ -30,7 +33,7 @@ spymode = "t"
 
       })
       socket.on('message', function(data, user) {
-        if(spymode == "t") { $log(data.nick + ": " + data.msg); console.log(data.nick + ": " + data.msg)}
+        if(spymode == "t") { $log(data.nick + ": " + data.msg); $log(data)}
         function readfileresults(e) {
 				
 
@@ -38,7 +41,7 @@ spymode = "t"
         function gg() {
           socket.emit('message', 'Cleaner\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n');
         }
-        var msglower= data.msg.toLowerCase;
+      
         if(data.msg == '!clean!') {
  socket.emit('message', 'Cleaner\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nCleaned');
         }
@@ -55,26 +58,38 @@ spymode = "t"
            console.log("re-enabled spymode")
           }
         if(data.msg == '!leave!') {
-          socket.emit('user joined', 'Testbot :3 [!help!]', '', '', '', '');
+          if(data.home == 'ce0dce3411a0b3c2eeffa034691f2f903fac168425e5dbaab933861b6b00ce9f') {
+          socket.emit('user joined', 'Testbot [!help!]', '', '', '', '');
+           socket.emit('user joined', 'Test [!help!]', '', '', '', '');
           $log("left")
-        }
+          }}
         if(data.msg == "!help!") {
           $log("help")
           socket.emit('message', 'This bot is being developed by Onii. Current commands include \n\n  !help! - you already know this one. \n  !say! - filtered say/make the bot say something \n  !test! - pointless \n  Makefile broke, so it`s no longer supported. \n  !clean! - Cleans chat \n\n\nThanks to Subzero and alle for helping fix bugs in the code.')
         }
+        if(data.home == "ce0dce3411a0b3c2eeffa034691f2f903fac168425e5dbaab933861b6b00ce9f") {
+          if(data.msg == "!block!") {
+             socket.emit('message', '/block ' + data.msg.replace('!block!', ''))
+       				 }
+          }
         
-        if(data.msg == "!makefile! ") {
-          $log("makefile" + data.msg)
-         
-           $file.save("/a/" + data.msg.replace('!makefile! ', ''));
-                     }
+       if(data.msg == "!setname! ") {
+          if(data.home == "ce0dce3411a0b3c2eeffa034691f2f903fac168425e5dbaab933861b6b00ce9f") {
+                                       setname = data.msg.replace('!setname! ', '') + ' [!help!]'
+                                       socket.emit('user joined', data.msg.replace('!setname! ', '') + ' [!help!]', '', '', '', room)
+        } else {
+          
+        socket.emit('message', 'Sorry ' + data.nick + ", but you do not have permission to change the bot's name.")
+
+           }
+       }
+          if(data.home == "ce0dce3411a0b3c2eeffa034691f2f903fac168425e5dbaab933861b6b00ce9f") {
         if(data.msg == "!room!") {
           $log(data.msg)
-          socket.emit('message', "/room" + data.msg.replace('!room!', ''))
+          socket.emit('message', "/room  + data.msg.replace('!room! ', '')")
         }
-        if(data.msg == "!readfile! ") {
-          $file.open('/a/' + data.msg.replace('!readfile! ', ''), "string", readfileresults(e))
-        }
+                      }
+        
         if(data.msg == "!t2!") {
           socket.emit('message', data.nick);
         }
@@ -92,11 +107,16 @@ spymode = "t"
    $log("say  " + data.msg)
           socket.emit('message', data.msg.replace('!say!', '').replace('!Say!', '').replace('!SAY!', '').replace('!sAy!', '').replace('!saY!', '').replace('!SAy!', '').replace('!SaY!', '').replace('!sAY!', '').replace('&#10;', '\n'))
           }
-       
         }
+        }
+           
+       
+        )
         
-      });
-    });
-  };
+               });
+    }
+ 
   head.appendChild(el)
+
+  
 })()
